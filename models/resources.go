e7caf06227cd26dc513454a20f72cc4a416ff909
Elhateknolojia <curtisombai@gmail.com>
2026-06-23 03:19:54 +0300

    working on resources

--- models/resources.go
+++ models/resources.go
@@ -4,11 +4,11 @@ import "time"
 
 type Resource struct {
     ID        string    `json:"id" bson:"_id"`
-    Title     string    `json:"title"`
-    Type      string    `json:"type"`
-    Audience  string    `json:"audience"`
-    FileName  string    `json:"fileName"`
-    FilePath  string    `json:"filePath"`
-    FileSize  int64     `json:"fileSize"`
-    DateAdded time.Time `json:"dateAdded"`
+    Title     string    `json:"title" bson:"title"`
+    Type      string    `json:"type" bson:"type"`
+    Audience  string    `json:"audience" bson:"audience"`
+    FileName  string    `json:"fileName" bson:"filename"`
+    FilePath  string    `json:"filePath" bson:"filepath"`
+    FileSize  int64     `json:"fileSize" bson:"filesize"`
+    DateAdded time.Time `json:"dateAdded" bson:"dateadded"`
 }
